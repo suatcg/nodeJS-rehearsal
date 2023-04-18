62e75b518bcaac33d7b413444b9b46941275e729
suatcg <suat-can_24@hotmail.com>
2023-04-18 23:24:43 +0300

    mvc-pattern enhancement

--- app.js
+++ app.js
@@ -3,28 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controller/error');
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const app = express();
-
-// Set the template engine that we use in express app by set function after install pug package into app
-// app.set('view engine', 'pug');
-app.set('view engine', 'ejs');
-
-app.set('views', 'views'); // Set the view ('template') files will be loaded
-
-app.use(bodyParser.urlencoded({ extended: true }));
-// insert the static file out of routes
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Filtering with /admin , so all routes has to starart with to execute the admin routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// It will work for unhandled routes
 app.use(errorController.get404);
 
 app.listen(3000, () => {
--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-	path.dirname(require.main.filename),
+	path.dirname(process.mainModule.filename),
 	'data',
 	'products.json'
 );
@@ -18,14 +18,19 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-	constructor(t) {
-		this.title = t;
+	constructor(title, imageUrl, description, price) {
+		this.title = title;
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.price = price;
 	}
 
 	save() {
 		getProductsFromFile((products) => {
 			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err);
+			});
 		});
 	}
 
