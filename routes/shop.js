2afdb643b93b04f7f63c1007a97a56183eeaf48f
suatcg <suat-can_24@hotmail.com>
2023-04-28 16:16:28 +0300

    route params and req.body used to pass information

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,12 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+// Put the dynamic routed to the end , in order to prevent missing routes below from it. Fot instance if we put below route like '/products/delete', this delete treate as a productId for that reason put to the end dynamic routes.
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
